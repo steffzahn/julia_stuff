b74e92a10c4fab21a4b54b7a27f4daa6bc9ddec1
steffzahn <steffzahn@gmail.com>
2021-08-01 21:40:06 +0200

    more colors

--- exper.jl
+++ exper.jl
@@ -62,25 +62,38 @@ function *((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64},
 end
 
 function myimage(x::Float64,y::Float64,z::Float64,u::Float64,
-                 radius::Float64,limit::Float64,colorsteps::Int64,size::Int64)
+                 radius::Float64,limit::Float64,size::Int64)
     image=Matrix{RGB}(UndefInitializer(),size,size)
     step = radius*2.0/convert(Float64,size)
-    gray = 1.0/convert(Float64,colorsteps)
+    colorstepsOneColor=256
+    colorsteps=6*colorstepsOneColor
+    gray = 1.0/convert(Float64,colorstepsOneColor)
+    colors=Array{RGB}(UndefInitializer(),colorsteps)
+    for ii in 1:colorstepsOneColor
+        scaledgray=gray*ii
+        colors[ii]=RGB(scaledgray,0.0,0.0)
+        colors[2*ii+colorstepsOneColor]=RGB(0.0,scaledgray,0.0)
+        colors[2*ii-1+colorstepsOneColor]=RGB(0.0,scaledgray,0.0)
+        colors[3*ii+3*colorstepsOneColor]=RGB(0.0,0.0,scaledgray)
+        colors[3*ii-1+3*colorstepsOneColor]=RGB(0.0,0.0,scaledgray)
+        colors[3*ii-2+3*colorstepsOneColor]=RGB(0.0,0.0,scaledgray)
+    end
+    black=RGB(0.0,0.0,0.0)
     xpos = x-radius
     for i in 1:size
         ypos = y-radius
         for j in 1:size
-            n=0
+            n=1
             c=(xpos,ypos,z,u)
             v=(0.0,0.0,0.0,0.0)
             while true
                 if norm(v)>=limit
                     color=gray*convert(Float64,n)
-                    image[i,j] = RGB(color,color,color)
+                    image[i,j] = colors[n]
                     break
                 end
                 if n>colorsteps-1
-                    image[i,j] = RGB(0.0,0.0,0.0)
+                    image[i,j] = black
                     break
                 end
                 n += 1
@@ -94,7 +107,7 @@ function myimage(x::Float64,y::Float64,z::Float64,u::Float64,
 end
 
 function mydraw(fn::String,x::Float64,y::Float64,z::Float64,u::Float64,
-                radius::Float64,limit::Float64,colorsteps::Int64,size::Int64)
-    image=myimage(x,y,z,u,radius,limit,colorsteps,size)
+                radius::Float64,limit::Float64,size::Int64)
+    image=myimage(x,y,z,u,radius,limit,size)
     save(fn,image)
 end
