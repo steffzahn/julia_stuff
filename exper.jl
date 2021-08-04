50ef34b53aabf3f8702f3776450225a66f8a9581
steffzahn <steffzahn@gmail.com>
2021-08-04 17:56:14 +0200

    clean out some functions

--- exper.jl
+++ exper.jl
@@ -6,33 +6,6 @@ import Base.one
 using Images
 using ColorTypes
 
-function zero((x1,x2,x3)::Tuple{Float64, Float64, Float64})
-    return (0.0,0.0,0.0)
-end
-
-function +((x1,x2,x3)::Tuple{Float64, Float64, Float64}, (y1,y2,y3)::Tuple{Float64, Float64, Float64})
-    return (x1+y1,x2+y2,x3+y3)
-end
-
-function -((x1,x2,x3)::Tuple{Float64, Float64, Float64}, (y1,y2,y3)::Tuple{Float64, Float64, Float64})
-    return (x1-y1,x2-y2,x3-y3)
-end
-
-function *((x1,x2,x3)::Tuple{Float64, Float64, Float64}, y::Float64)
-    return (x1*y,x2*y,x3*y)
-end
-function *(y::Float64, (x1,x2,x3)::Tuple{Float64, Float64, Float64})
-    return (x1*y,x2*y,x3*y)
-end
-
-function norm((x1,x2,x3)::Tuple{Float64, Float64, Float64})
-    return sqrt(x1*x1+x2*x2+x3*x3)
-end
-
-function *((x1,x2,x3)::Tuple{Float64, Float64, Float64}, (y1,y2,y3)::Tuple{Float64, Float64, Float64})
-    return (x2*y3+x3*y2-2*x1*y1,-x1*y3-x3*y1+2*x2*y2,x1*y2+x2*y1-2*x3*y3)
-end
-
 function zero((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64})
     return (0.0,0.0,0.0,0.0)
 end
