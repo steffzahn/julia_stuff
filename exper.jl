5632a766b5270d49ec4935b2241c99b0b4478db2
steffzahn <steffzahn@gmail.com>
2021-08-08 19:31:05 +0200

    fix multiplication

--- exper.jl
+++ exper.jl
@@ -46,9 +46,9 @@ end
 function *((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64},
            (y1,y2,y3,y4)::Tuple{Float64, Float64, Float64, Float64})
     return (x1*y1-x2*y2-x3*y3-x4*y4,
-            x2*y1+x1*y2+x3*y4+x4*y3,
-            x3*y1+x1*y3+x2*y4+x4*y2,
-            x1*y4+x4*y1+x2*y3+x3*y2)
+            x2*y1+x1*y2+x3*y4-x4*y3,
+            x3*y1+x1*y3-x2*y4+x4*y2,
+            x1*y4+x4*y1+x2*y3-x3*y2)
 end
 
 function initPalette(;colorScheme::Int64=0)::Tuple{Vector{RGB},Int64}
