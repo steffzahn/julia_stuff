299fb24d7a968ad78fb9acd1b72935d023b79896
steffzahn <steffzahn@gmail.com>
2021-08-09 09:14:28 +0200

    dot product

--- exper.jl
+++ exper.jl
@@ -6,36 +6,52 @@ import Base.one
 using Images
 using ColorTypes
 
-function zero((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64})
+function zero((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64}
+              )::Tuple{Float64, Float64, Float64, Float64}
     return (0.0,0.0,0.0,0.0)
 end
-function one((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64})
+
+function one((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64}
+             )::Tuple{Float64, Float64, Float64, Float64}
     return (1.0,0.0,0.0,0.0)
 end
+
 function +((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64},
-           (y1,y2,y3,y4)::Tuple{Float64, Float64, Float64, Float64})
+           (y1,y2,y3,y4)::Tuple{Float64, Float64, Float64, Float64}
+           )::Tuple{Float64, Float64, Float64, Float64}
     return (x1+y1,x2+y2,x3+y3,x4+y4)
 end
 
 function -((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64},
-           (y1,y2,y3,y4)::Tuple{Float64, Float64, Float64, Float64})
+           (y1,y2,y3,y4)::Tuple{Float64, Float64, Float64, Float64}
+           )::Tuple{Float64, Float64, Float64, Float64}
     return (x1-y1,x2-y2,x3-y3, x4-y4)
 end
-function -((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64})
+function -((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64}
+           )::Tuple{Float64, Float64, Float64, Float64}
     return (-x1,-x2,-x3,-x4)
 end
 
-function *((x1,x2,x3, x4)::Tuple{Float64, Float64, Float64, Float64}, y::Float64)
+function *((x1,x2,x3, x4)::Tuple{Float64, Float64, Float64, Float64}, y::Float64
+           )::Tuple{Float64, Float64, Float64, Float64}
     return (x1*y,x2*y,x3*y,x4*y)
 end
-function *(y::Float64, (x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64})
+function *(y::Float64, (x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64}
+           )::Tuple{Float64, Float64, Float64, Float64}
     return (x1*y,x2*y,x3*y,x4*y)
 end
 
-function norm((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64})
-    return sqrt(x1*x1+x2*x2+x3*x3+x4*x4)
+# \cdot tab
+function ⋅((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64},
+          (y1,y2,y3,y4)::Tuple{Float64, Float64, Float64, Float64})::Float64
+    return x1*y1+x2*y2+x3*y3+x4*y4
 end
-function normalize(a::Tuple{Float64, Float64, Float64, Float64})
+
+function norm(x::Tuple{Float64, Float64, Float64, Float64})::Float64
+    return sqrt(x ⋅ x)
+end
+function normalize(a::Tuple{Float64, Float64, Float64, Float64}
+                   )::Tuple{Float64, Float64, Float64, Float64}
     n=norm(a)
     if n==0.0
         return a
@@ -44,7 +60,8 @@ function normalize(a::Tuple{Float64, Float64, Float64, Float64})
 end
 
 function *((x1,x2,x3,x4)::Tuple{Float64, Float64, Float64, Float64},
-           (y1,y2,y3,y4)::Tuple{Float64, Float64, Float64, Float64})
+           (y1,y2,y3,y4)::Tuple{Float64, Float64, Float64, Float64}
+           )::Tuple{Float64, Float64, Float64, Float64}
     return (x1*y1-x2*y2-x3*y3-x4*y4,
             x2*y1+x1*y2+x3*y4-x4*y3,
             x3*y1+x1*y3-x2*y4+x4*y2,
