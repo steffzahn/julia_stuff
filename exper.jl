1a243648168dcb708eb63fa3dd5cd310dc33692f
steffzahn <steffzahn@gmail.com>
2022-01-02 13:21:26 +0100

    triangle

--- exper.jl
+++ exper.jl
@@ -171,8 +171,8 @@ function myimage((x,y,z,u)::Tuple{Float64, Float64, Float64, Float64},
                 end
                 n += 1
                 vtemp = v
-                v = v * v * (1.0/11.0) + w + c
-                w = w * w * w * (1.0/3.0) - vtemp * 1.02 + c
+                v = v * v * (1.0/73.0) + w + c
+                w = w * w * vtemp * (1.0/7.0) - vtemp + c
             end
             ypos += step
         end
