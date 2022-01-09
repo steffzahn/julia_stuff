1583361b83b881410040b67154a986649dcddd81
steffzahn <steffzahn@gmail.com>
2022-01-09 18:54:57 +0100

    ring

--- exper.jl
+++ exper.jl
@@ -181,8 +181,8 @@ function myimage((x,y,z,u)::Tuple{Float64, Float64, Float64, Float64},
                 if isnan(vinv)
                     vinv = zero(vinv)
                 end
-                v = v * v * v * (1.0/8.0) + w + c
-                w = w * w + vinv * (1.0/700.0) - vtemp + c
+                v = v * v * (1.0/11.0) + w + c
+                w = vinv * vinv * vinv * (1.0/37.0) - vtemp + c
             end
             ypos += step
         end
