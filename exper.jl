68331e653fc8e46b5b50d98f0dc5f50738ec4c95
steffzahn <steffzahn@gmail.com>
2022-01-15 17:54:42 +0100

    bubbles

--- exper.jl
+++ exper.jl
@@ -181,8 +181,9 @@ function myimage((x,y,z,u)::Tuple{Float64, Float64, Float64, Float64},
                 if isnan(vinv)
                     vinv = zero(vinv)
                 end
-                v = v * v * (1.0/23.0) + w + c
-                w = vinv * vinv * vinv * (1.0/5.0) + vtemp + c
+                v = v * v * v * (1.0/5557.0) + w + c
+                t=w-vinv
+                w = t * t * t * t * (1.0/5531.0) + vtemp + c
             end
             ypos += step
         end
